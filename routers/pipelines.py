19e31fa04f331d80cafcfe01c85509458006bd10
Marina Hernández Bautista <marinahbau@ugr.es>
2025-07-02 12:13:42 +0200

    Update pipelines: fit, predict and decision_function

--- routers/pipelines.py
+++ routers/pipelines.py
@@ -18,6 +18,13 @@ from SADL.static_data.algorithms import sklearn
 from SADL.time_series.algorithms import tsfedl
 from SADL.federated_data.algorithms import flexanomalies
 
+#Preprocessing
+from SADL.static_data.preprocessing.preprocessing_static import preprocessing_static_algorithms
+from SADL.time_series.preprocessing.preprocessing_ts import preprocessing_ts_algorithms
+
+# Visualization
+from SADL.visualization_module import DataVisualization
+
 import numpy as np
 
 router = APIRouter()
@@ -49,7 +56,7 @@ async def run_pipeline(request: Request):
         node = nodes[node_id]
         node_category = node.get("category", "")
         node_type = node["op_type"]
-        model_type = node.get("model_type", "")
+        model_type = node.get("model", "")
         params = node.get("params", {})
         print(f"Processing node: {node}")
 
@@ -58,51 +65,110 @@ async def run_pipeline(request: Request):
 
             if(node_category == "static_data"):
                 X, y = global_load_static(params["dataset"])
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,stratify=y, random_state=42)
+                context["X_train"] = X_train
+                context["y_train"] = y_train
+                context["X_test"] = X_test
+                context["y_test"] = y_test
+                print(f"Dataset loaded: {params['dataset']}")
+
             elif(node_category == "time_series"):
                 # Placeholder for time series dataset loading logic
                 X, y = global_load_ts(params["dataset"])
                 #TODO: what to do with this data division
                 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,stratify=y, random_state=42)
-                print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-            #context["X"] = X
-            #context["y"] = y
-        elif node_type == "ModelSetup":
-            if(model_type in pyod_algorithms):
+                context["X_train"] = X_train
+                context["y_train"] = y_train
+                context["X_test"] = X_test
+                context["y_test"] = y_test
+                print(f"Dataset loaded: {params['dataset']}")
+
+        elif node_type == "Preprocessing":
+            print(f"Preprocessing...{model_type}")
+            X_train = context.get("X_train")
+            X_test = context.get("X_test")
+            print(f"Current dataset: {X}")
+            if X_train is None:
+                raise ValueError("No dataset loaded before preprocessing")
+
+            ScalerClass = None
+            if (model_type in preprocessing_static_algorithms):
+                ScalerClass = preprocessing_static_algorithms.get(model_type)
+            elif (model_type in preprocessing_ts_algorithms):
+                ScalerClass = preprocessing_ts_algorithms.get(model_type)
+                
+            if ScalerClass is None:
+                return {"error": f"Unknown preprocessing model_type: {model_type}"}
+            
+            scaler_instance = ScalerClass()
+            X_scaled = scaler_instance.fit_transform(X_train)
+            X_test_scaled = scaler_instance.transform(X_test)
+            context["X_train"] = X_scaled
+            context["X_test"] = X_test_scaled
+            print(f"Preprocessing done: {scaler_instance}")
+
+        elif node_type == "Model Setup":
+            print(f"Setting up model: {params['algorithm_']}")
+            if(params['algorithm_'] in pyod_algorithms):
                 kwargs = params
                 model = pyod.PyodAnomalyDetection(**kwargs)
                 print(f"Model initialized: {model}")
-            elif(model_type in sklearn_algorithms):
+                context["model"] = model
+
+            elif(params['algorithm_'] in sklearn_algorithms):
                 kwargs = params
                 model = sklearn.SkLearnAnomalyDetection(**kwargs)
                 print(f"Model initialized: {model}")
-            elif(model_type in tsfedl_algorithms):
+                context["model"] = model
+
+            elif(params['algorithm_'] in tsfedl_algorithms):
                 kwargs = params
                 model = tsfedl.TsfedlAnomalyDetection(**kwargs)
                 print(f"Model initialized: {model}")
-            elif(model_type in flexanomalies_algorithms):
+                context["model"] = model
+
+            elif(params['algorithm_'] in flexanomalies_algorithms):
                 kwargs = params
                 model = flexanomalies.FlexAnomalyDetection(**kwargs)
                 print(f"Model initialized: {model}")
+                context["model"] = model
 
-        elif node_type == "FitModel":
+        elif node_type == "Fit Model":
             model = context.get("model")
-            X = context.get("X")
+            X = context.get("X_train")
 
-            # Clean X
-            X = X.replace([np.inf, -np.inf], np.nan).dropna()
-            context["X"] = X  # update X after cleaning
-            context["model"].fit(X)
-            scores = context["model"].decision_function(X) * -1
-            context["scores"] = scores.tolist()
+            model.fit(X)
+            print(f"Model fitted: {model}")
+
+        elif node_type == "Decision Function Model":
+            model = context.get("model")
+            X_train = context.get("X_train")
+            scores_pred = model.decision_function(X_train)* -1
+            print("Scores",scores_pred)
+
+        elif node_type == "Predict Model":
+            model = context.get("model")
+            X_test = context.get("X_test")
+
+            if model is None:
+                raise ValueError("Model not set up before prediction")
+
+            pred = model.predict(X_test)
+            print(f"Prediction: {pred}")
 
         elif node_type == "Visualize":
-            # Dummy output — in real use, return a plot or metrics
-            return {
-                "message": "Pipeline executed successfully",
-                "scores": context.get("scores", [])
-            }
+            np.random.seed(42)
+            data = np.random.rand(100,5)   
+
+
+            vis = DataVisualization(data, plot_technique='heatmap', heatmap_color='viridis') 
+            vis.fit()  
+            json = vis.to_json()    
+            print(f"Visualization created: {json}")   
+            #visualizer = DataVisualization(data=None, plot_technique='anomaly_labels', y_true=true[40:60], y_pred=pred[40:60])
+
 
         else:
             return {"error": f"Unknown node type: {node_type}"}
 
-    return {"message": "Pipeline executed, but no visualize node found"}
+    return {"message": "Pipeline executed"}
